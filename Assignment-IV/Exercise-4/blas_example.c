0ddcaf13f9feb76726caafae2d5eec9362c6a48d
Daniel Pfister <dan.pfister27@gmail.com>
2025-05-19 08:40:28 +0200

    updated code for ex4

--- Assignment-IV/Exercise-4/blas_example.c
+++ Assignment-IV/Exercise-4/blas_example.c
@@ -3,9 +3,7 @@
 #include <cblas.h>
 #include <mpi.h>
 
-#define N 1000  // Matrix size
-
-void initialize(double *matrix, double *vector) {
+void initialize(double *matrix, double *vector, int N) {
     for (int i = 0; i < N * N; i++) {
         matrix[i] = (double)(i % 100) / 10.0;
     }
@@ -15,33 +13,44 @@ void initialize(double *matrix, double *vector) {
 }
 
 int main(int argc, char* argv[]) {
+    int N = 1000;
     int rank, n_ranks, provided, iter, n_rows;
     double start_time, elapsed_time;
+
+    if (argc > 1) {
+        N = atoi(argv[1]);
+    }
 
     MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
     start_time = MPI_Wtime();
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
 
+    if (N%n_ranks != 0) {
+        printf("N (%d) not divisible by n_ranks (%d)", N, n_ranks);
+        MPI_Finalize();
+        return EXIT_FAILURE;
+    }
+
     n_rows = N / n_ranks; // num of rows for each process -> assume is nicely divisible
-    iter = rank * n_rows; // start of row for each process
+    iter = rank * n_rows; // start index of row for each process
 
     double *vector = (double*) malloc(N * sizeof(double)); // all processes need alloc for vector
     double *local_matrix = (double*) malloc(n_rows * N * sizeof(double));
     double *local_result = (double*) malloc(n_rows * sizeof(double));
-    double* matrix = NULL;
+    double *matrix = NULL;
     double *result = NULL;
 
     if (rank == 0) { // only process 0 needs whole matrix and result
         matrix = (double*) malloc(N * N * sizeof(double));
         result = (double*) malloc(N * sizeof(double));
-        initialize(matrix, vector);
+        initialize(matrix, vector, N);
     }
     
     // cast vector from process 0 to all others
     MPI_Bcast(vector, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     // scatter matrix among all other processes
-    MPI_Scatter(matrix, n_rows * N, MPI_DOUBLE, &local_matrix, n_rows * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatter(matrix, n_rows * N, MPI_DOUBLE, local_matrix, n_rows * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     
     // Perform matrix-vector multiplication using BLAS
     cblas_dgemv(CblasRowMajor, CblasNoTrans, n_rows, N, 1.0, local_matrix, N, vector, 1, 0.0, local_result, 1);
@@ -57,19 +66,18 @@ int main(int argc, char* argv[]) {
         }
         fclose(f);
     }
-    
-    free(matrix);
-    free(local_matrix);
-    free(vector);
-    free(result);
-    free(local_result);
 
     elapsed_time = MPI_Wtime() - start_time;
 
     if (rank == 0) {
-        printf("BLAS matrix-vector multiplication complete.\n");
-        printf("#Ranks: %d, Execution time: %f\n", n_ranks, elapsed_time);
+        printf("BLAS MVM done (check: %f). N: %d, #Processes: %d, Execution time: %f\n", result[0], N, n_ranks, elapsed_time);
+        free(matrix);
+        free(result);
     }
+    
+    free(local_matrix);
+    free(vector);
+    free(local_result);
 
     MPI_Finalize();
     return 0;
--- Assignment-IV/Exercise-4/script_blas_mvm.sh
+++ Assignment-IV/Exercise-4/script_blas_mvm.sh
@@ -5,8 +5,14 @@
 #SBATCH -t 1:00:00
 #SBATCH -A edu25.DD2356
 #SBATCH -p shared
-#SBATCH --ntasks=10
+#SBATCH --ntasks=32
 #SBATCH --cpus-per-task=2
 #SBATCH --nodes=1
 
-srun -n 10 ./blas_mvm
+for N in 1000 2000 4000 5000 8000 10000 16000; do
+    srun -n 8 ./blas_mvm $N
+done
+
+for p in 1 2 4 8 16 32; do
+    srun -n $p ./blas_mvm 4000
+done
